ff64330bb6d929830877deddd7d35f1562a309f8
SND\gfinzer_cp <SND\gfinzer_cp@a83551a4-30f6-4d81-a974-c6ced450ddbf>
2009-06-01 16:07:28 +0000

    Initial Version

---
+++ Compare-NET-Objects-Tests/TestClasses/Deck.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KellermanSoftware.CompareNETObjectsTests
+{
+    public enum Deck : int
+    {
+        Engineering,
+        SickBay,
+        AstroPhysics
+    }
+}
---
+++ Compare-NET-Objects-Tests/TestClasses/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KellermanSoftware.CompareNETObjectsTests
+{
+    [Serializable]
+    public class Person
+    {
+        public DateTime DateCreated;
+
+        public string Name
+        {
+            get;
+            set;
+        }
+    }
+}
